2142563eaa216f3c6e0b6034065c4d37dd9ad9b0
jmoypace <jmoypace@gmail.com>
2018-05-03 21:04:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var katzDeliLine = [];
 function takeANumber(katzDeliLine,name){
   katzDeliLine.push(name)
   
-  return `Welcome, ${name}. You are number ${katzDeliLine.indexOf(name)} in line`
+  return `Welcome, ${name}. You are number ${katzDeliLine.indexOf(name)+1} in line`
  
 
 
