437299d4b5b12cd84a660a54aaac79852e8f5fe9
jmoypace <jmoypace@gmail.com>
2018-05-03 20:58:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
  
 
-function deli(){ 
-  var katzDeli = [];
+
 function takeANumber(katzDeliLine,name){
  katzDeliLine.push(name)
  for(let i=0;i<katzDeliLine.length;i++){
@@ -10,5 +9,5 @@ function takeANumber(katzDeliLine,name){
   
   
 }
-}
+
 }
