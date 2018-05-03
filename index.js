40f4b497bd8481576038d05bde3c33aa3b4daf85
jmoypace <jmoypace@gmail.com>
2018-05-03 21:14:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function nowServing(){
     return "There is nobody waiting to be served!"
   }
   else {
-    console.log(`Currently serving ${katzDeliLine}`)
+    return(`Currently serving ${katzDeliLine}`)
   }
   
 }
