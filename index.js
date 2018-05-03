4ba213a1f1bb18be49e9ba509b5ef1bf54c19ab9
jmoypace <jmoypace@gmail.com>
2018-05-03 21:27:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function takeANumber(katzDeliLine,name){
  
   return `Welcome, ${name}. You are number ${katzDeliLine.indexOf(name)+1} in line.`
 }
-function nowServing(){
+function nowServing(katzDeliLine){
   if(katzDeliLine.length===0){
     return "There is nobody waiting to be served!"
   }
