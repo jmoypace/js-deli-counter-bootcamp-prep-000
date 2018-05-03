3c2560c3ce9b29c1adc81c971dbb752be5c11193
jmoypace <jmoypace@gmail.com>
2018-05-03 21:10:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function takeANumber(katzDeliLine,name){
   return `Welcome, ${name}. You are number ${katzDeliLine.indexOf(name)+1} in line.`
 }
 function nowServing(){
-  if(katzDeliLine.length=0){
-    return "no one is on line"
+  if(katzDeliLine.length===0){
+    return "There is nobody waiting to be served!"
   }
 }
